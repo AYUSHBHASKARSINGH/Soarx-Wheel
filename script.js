14522fb4594adae47a4d3f8b221aad6e9cb1b8c0
AYUSHBHASKARSINGH <95188806+AYUSHBHASKARSINGH@users.noreply.github.com>
2023-12-27 00:44:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,89 +1,44 @@
-let rotationAnimation;
-let rotationSpeed = 5; // Default rotation speed
+let rotateClockwise = true;
 
 function startRotation() {
-  document.querySelector('.outer-circle').style.animation = `rotateAnimation ${rotationSpeed}s linear infinite`;
+  const outerCircle = document.querySelector('.outer-circle');
+  outerCircle.style.transition = 'transform 0.3s ease-in-out';
+  outerCircle.style.transform = 'rotate(45deg)';
+  displayRotationMessage("Wheel is rotating!");
+  updateHoverText("Wheel is rotating!");
 }
 
 function stopRotation() {
-  document.querySelector('.outer-circle').style.animation = 'none';
+  const outerCircle = document.querySelector('.outer-circle');
+  outerCircle.style.transition = 'transform 0.3s ease-in-out';
+  outerCircle.style.transform = 'rotate(0deg)';
+  displayRotationMessage("");
+  updateHoverText("Hover to Rotate");
 }
 
-function reverseRotation() {
-  document.querySelector('.outer-circle').style.animation = `rotateAnimation ${rotationSpeed}s linear infinite reverse`;
-}
-
-function showSurprise() {
-  createFireworks();
-}
-
-function createFireworks() {
-  const canvas = document.getElementById('fireworksCanvas');
-  const ctx = canvas.getContext('2d');
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
-
-  const particles = [];
-
-  function Particle() {
-    this.x = Math.random() * canvas.width;
-    this.y = canvas.height;
-    this.size = Math.random() * 10 + 2;
-    this.speedX = Math.random() * 3 - 1.5;
-    this.speedY = Math.random() * -15 - 1;
-    this.color = `hsla(${Math.random() * 360}, 100%, 50%, 1)`;
-  }
-
-  Particle.prototype.update = function () {
-    this.x += this.speedX;
-    this.y += this.speedY;
-    if (this.size > 0.2) this.size -= 0.1;
-  };
-
-  Particle.prototype.draw = function () {
-    ctx.fillStyle = this.color;
-    ctx.beginPath();
-    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
-    ctx.fill();
-  };
-
-  function createParticles() {
-    const particle = new Particle();
-    particles.push(particle);
-  }
-
-  function animateParticles() {
-    for (let i = particles.length - 1; i >= 0; i--) {
-      if (particles[i].size <= 0.2) {
-        particles.splice(i, 1);
-      } else {
-        particles[i].update();
-        particles[i].draw();
-      }
-    }
-  }
-
-  function animate() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    createParticles();
-    animateParticles();
-    requestAnimationFrame(animate);
-  }
-
-  animate();
-
-  // Stop fireworks after 2 seconds
-  setTimeout(() => {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-  }, 2000);
-}
-
-function increaseSpeed() {
-  rotationSpeed += 1;
+function toggleRotationDirection() {
+  rotateClockwise = !rotateClockwise;
   startRotation();
 }
 
-function decreaseSpeed() {
-  rotationSpeed = Math.max(1, rotationSpeed - 1);
-  startRotation();
+function changeCircleSize(value) {
+  const outerCircle = document.querySelector('.outer-circle');
+  const innerCircle = document.querySelector('.inner-circle');
+
+  outerCircle.style.width = `${value}px`;
+  outerCircle.style.height = `${value}px`;
+
+  const innerCircleSize = value / 2;
+  innerCircle.style.width = `${innerCircleSize}px`;
+  innerCircle.style.height = `${innerCircleSize}px`;
+}
+
+function displayRotationMessage(message) {
+  const rotationMessage = document.getElementById('rotationMessage');
+  rotationMessage.textContent = message;
+}
+
+function updateHoverText(text) {
+  const hoverText = document.getElementById('hoverText');
+  hoverText.textContent = text;
 }
